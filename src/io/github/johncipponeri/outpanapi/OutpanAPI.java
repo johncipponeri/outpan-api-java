f6b41e334dc2739c15bd7c14c161b6f21ee1b882
John Cipponeri <john.cipponeri@gmail.com>
2015-06-10 14:01:53 -0500

    Initial commit.

---
+++ src/io/github/johncipponeri/outpanapi/OutpanAPI.java
@@ -0,0 +1,76 @@
+package io.github.johncipponeri.outpanapi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
+
+import org.json.JSONObject;
+
+public class OutpanAPI {
+	
+	private String api_key;
+	
+	public OutpanAPI(String api_key) {
+		this.api_key = api_key;
+	}
+	
+	private JSONObject executeGet(String barcode) {
+		return executeGet(barcode, "");
+	}
+	
+	private JSONObject executeGet(String barcode, String endpoint) {
+		JSONObject jsonResult = new JSONObject();
+		
+		try {
+			URL url = new URL("https://api.outpan.com/v1/products/" + barcode + endpoint);
+			URLConnection uc = url.openConnection();
+			
+			String key = api_key + ":";
+			String basicAuth = "Basic " + new String(Base64.getEncoder().encode(key.getBytes()));
+			
+			uc.setRequestProperty ("Authorization", basicAuth);
+			
+			InputStream in = uc.getInputStream();
+			InputStreamReader isr = new InputStreamReader(in);
+			
+			int numCharsRead;
+			char[] charArray = new char[1024];
+			StringBuffer sb = new StringBuffer();
+			
+			while ((numCharsRead = isr.read(charArray)) > 0)
+				sb.append(charArray, 0, numCharsRead);
+
+			jsonResult = new JSONObject(sb.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return jsonResult;
+	}
+	
+	public OutpanObject getProduct(String barcode) {
+		return new OutpanObject(executeGet(barcode));
+	}
+	
+	public OutpanObject getProductName(String barcode) {
+		return new OutpanObject(executeGet(barcode, "/name"));
+	}
+	
+	public OutpanObject getProductAttributes(String barcode) {
+		return new OutpanObject(executeGet(barcode, "/attributes"));
+	}
+	
+	public OutpanObject getProductImages(String barcode) {
+		return new OutpanObject(executeGet(barcode, "/images"));
+	}
+	
+	public OutpanObject getProductVideos(String barcode) {
+		return new OutpanObject(executeGet(barcode, "/videos"));
+	}
+}
---
+++ src/io/github/johncipponeri/outpanapi/OutpanObject.java
@@ -0,0 +1,54 @@
+package io.github.johncipponeri.outpanapi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class OutpanObject {
+
+	public String
+		gtin,
+		outpan_url,
+		name;
+	
+	public HashMap<String, String>
+		attributes;
+	
+	public ArrayList<String>
+		images,
+		videos;
+	
+	public OutpanObject() {
+		this.gtin = "";
+		this.outpan_url = "";
+		this.name = "";
+		
+		this.attributes = new HashMap<String, String>();
+		this.images = new ArrayList<String>();
+		this.videos = new ArrayList<String>();
+	}
+	
+	public OutpanObject(JSONObject json) {
+		this();
+		
+		this.gtin = json.getString("gtin");
+		this.outpan_url = json.getString("outpan_url");
+		this.name = json.getString("name");
+		
+		JSONObject attrObject = json.getJSONObject("attributes");
+		String[] attrs = JSONObject.getNames(attrObject);
+		
+		for (int a = 0; a < attrs.length; a++)
+			this.attributes.put(attrs[a], attrObject.getString(attrs[a]));
+		
+		JSONArray imgs = json.getJSONArray("images");
+		for (int i = 0; i < imgs.length(); i++)
+			this.images.add(imgs.getString(i));
+		
+		JSONArray vids = json.getJSONArray("videos");
+		for (int i = 0; i < vids.length(); i++)
+			this.videos.add(vids.getString(i));
+	}
+}
